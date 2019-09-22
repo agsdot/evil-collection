3480885f11358c4adc5dba0cd8bf7af36e333b2a
Jonathan Lai <jonathanlai@gmail.com>
2019-09-21 23:29:23 -0700

    Test unimpaired again

--- evil-collection.el
+++ evil-collection.el
@@ -572,9 +572,10 @@ instead of the modes in `evil-collection-mode-list'."
             (run-hook-with-args 'evil-collection-setup-hook
                                 m mode-keymaps))))))
   (when evil-collection-want-unimpaired-p
-    (load (expand-file-name "modes/unimpaired/evil-collection-unimpaired"
-                            evil-collection-base-dir)
-          nil t)
+    (require 'evil-collection-unimpaired)
+    ;;(load (expand-file-name "modes/unimpaired/evil-collection-unimpaired"
+    ;;                        evil-collection-base-dir)
+    ;;      nil t)
     (when (fboundp 'evil-collection-unimpaired-setup)
       (evil-collection-unimpaired-setup))))
 
